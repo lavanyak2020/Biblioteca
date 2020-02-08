b6f840e64349ca1cfe24fcc5f63d9f520de53ed2
Lavanya K <lavanya.konda@thoughtworks.com>
2020-02-08 22:28:31 +0530

    [Lavanya K] #1.6 Add. Test Quit option

--- src/test/java/com/twu/biblioteca/QuitOptionTest.java
+++ src/test/java/com/twu/biblioteca/QuitOptionTest.java
@@ -1,5 +1,19 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.twu.biblioteca;
+
+import UI.AppUI;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
 
 class QuitOptionTest {
 
+    @Test
+    public void shouldInvokeQuitApplicationWhenExecuted() {
+        AppUI appUI = mock(AppUI.class);
+        QuitOption quitOption = QuitOption.createQuitOption(appUI);
+
+        quitOption.execute();
+
+        verify(appUI, times(1)).quitApplication();
+    }
 }
