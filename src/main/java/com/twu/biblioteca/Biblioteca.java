376a5b860f4614c8e3d0547e6df65089e1b39f21
Lavanya K <lavanya.konda@thoughtworks.com>
2020-02-08 21:26:49 +0530

    [Lavanya K] #1.4 Add. Add Menu to Biblioteca

--- src/main/java/com/twu/biblioteca/Biblioteca.java
+++ src/main/java/com/twu/biblioteca/Biblioteca.java
@@ -1,19 +1,19 @@
 package com.twu.biblioteca;
 
-import java.util.List;
+import UI.AppUI;
 
 public class Biblioteca {
     private final String WELCOME_MESSAGE = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+    private final AppUI appUI;
     private Library library;
+    private Menu menu;
 
-    public Biblioteca() {
+    public Biblioteca(AppUI appUI) {
         library = new Library();
+        this.appUI = appUI;
+        menu = new Menu(library,appUI);
     }
     public String getWelcomeMessage() {
         return WELCOME_MESSAGE;
-    }
-
-    public List<Book> getListOfBooks() {
-        return library.getBooks();
     }
 }
