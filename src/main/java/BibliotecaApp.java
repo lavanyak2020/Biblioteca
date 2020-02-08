a4e811ae56fcf0294aaa296ccdd96423e14d4405
Lavanya K <lavanya.konda@thoughtworks.com>
2020-02-09 00:26:07 +0530

    [Lavanya K] #1.7 Refactor. Move Biblioteca declaration inside method in App

--- src/main/java/BibliotecaApp.java
+++ src/main/java/BibliotecaApp.java
@@ -2,16 +2,13 @@ import UI.AppUI;
 import com.twu.biblioteca.Biblioteca;
 
 public class BibliotecaApp {
-    private Biblioteca biblioteca;
-    private AppUI appUI;
-
     public static void main(String[] args) {
         new BibliotecaApp().start();
     }
 
     private void start() {
-        appUI = new AppUI();
-        biblioteca = new Biblioteca(appUI);
+        AppUI appUI = new AppUI();
+        Biblioteca biblioteca = new Biblioteca(appUI);
         appUI.displayWelcomeMessage(biblioteca.getWelcomeMessage());
         do {
             appUI.displayMenu(biblioteca.getMenuItems());
