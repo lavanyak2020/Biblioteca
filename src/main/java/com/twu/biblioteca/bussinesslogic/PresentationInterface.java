76b3d437a8afcc9d0f450f275ed5a43d002ca2f6
Lavanya K <lavanya.konda@thoughtworks.com>
2020-02-12 09:55:41 +0530

    [Lavanya K] Refactor. Add build pipeline

--- src/main/java/com/twu/biblioteca/bussinesslogic/PresentationInterface.java
+++ src/main/java/com/twu/biblioteca/bussinesslogic/PresentationInterface.java
@@ -3,6 +3,7 @@ package com.twu.biblioteca.bussinesslogic;
 import java.util.List;
 
 public interface PresentationInterface {
+
     void listOfBooks(List<Book> books);
 
     void quitApplication();
